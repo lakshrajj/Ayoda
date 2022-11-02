90efe8595c6abede731de50dd9d0c41e2c80be86
lakshrajj <lakshrajme@gmail.com>
2022-11-03 01:02:57 +0530

    Added two activities for SplashScreen and PostActivity.

--- app/src/main/java/com/luxx/ayoda/MainActivity.kt
+++ app/src/main/java/com/luxx/ayoda/MainActivity.kt
@@ -2,10 +2,17 @@ package com.luxx.ayoda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        postBtn.setOnClickListener {
+            val intent = Intent(this, PostActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
